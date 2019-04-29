c194cd78247e93211328d29c9fa28ca3bfc36abf
jamespeace <james.happylove@gmail.com>
2019-04-28 23:36:16 -0700

    [Hog] Implement problem 06.

--- hw/hog/hog.py
+++ hw/hog/hog.py
@@ -128,7 +128,8 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
-    "*** YOUR CODE HERE ***"
+        "*** YOUR CODE HERE ***"
+        say = say(score0, score1)
     # END PROBLEM 6
     return score0, score1
 
