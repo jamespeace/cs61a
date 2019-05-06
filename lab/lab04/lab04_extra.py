e0922dbaedce0af5613b77703bf0a834d523ea54
jamespeace <james.happylove@gmail.com>
2019-05-05 17:13:14 -0700

    [Lab04] Initial update.

---
+++ lab/lab04/lab04_extra.py
@@ -0,0 +1,28 @@
+""" Lab 04 Optional Questions """
+
+from lab04 import *
+
+
+this_file = __file__
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check(this_file, 'hailstone',
+    ...       ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
