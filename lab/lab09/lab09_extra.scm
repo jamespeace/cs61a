47cdbdf4c1c3045e338b48407ad01d0a9db80a8d
jamespeace <james.happylove@gmail.com>
2019-05-26 00:00:03 -0700

    [Lab09]Implement extra questions.

--- lab/lab09/lab09_extra.scm
+++ lab/lab09/lab09_extra.scm
@@ -2,15 +2,27 @@
 
 
 (define lst
-  'YOUR-CODE-HERE
+  (cons 
+    (cons 1 nil)
+    (cons
+      2
+      (cons 
+        (cons 3 (cons 4 nil))
+        (cons 5 nil)
+      )
+    )
+  )
 )
 
 (define (composed f g)
-  'YOUR-CODE-HERE
+  (lambda (x) (f (g x)))
 )
 
 (define (remove item lst)
-  'YOUR-CODE-HERE
+  (if (null? lst)
+    nil
+    (filter (lambda (x) (not(= x item))) lst)
+  )
 )
 
 
@@ -22,14 +34,24 @@
 (remove 5 '(5 3 5 5 1 4 5 4))
 ; expect (3 1 4 4)
 
+
 (define (no-repeats s)
-  'YOUR-CODE-HERE
+  (if (null? s)
+    s
+    (cons (car s) (no-repeats (remove (car s) (cdr s)))))
 )
 
 (define (substitute s old new)
-  'YOUR-CODE-HERE
+  (cond 
+  ((null? s) s)
+  ((pair? (car s)) (cons (substitute (car s) old new) (substitute (cdr s) old new)))
+  ((eq? old (car s)) (cons new (substitute (cdr s) old new)))
+  (else (cons (car s) (substitute (cdr s) old new)))
+  )
 )
 
 (define (sub-all s olds news)
-  'YOUR-CODE-HERE
+  (if (null? olds)
+    s
+    (sub-all (substitute s (car olds) (car news)) (cdr olds) (cdr news)))
 )
