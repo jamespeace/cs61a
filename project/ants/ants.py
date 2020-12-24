269700b9127420c0aafe7d40376faca5a7e13b15
dennis <dennis.huang@gmail.com>
2020-12-24 22:30:22 +0800

    ants: Implement problem 2

--- project/ants/ants.py
+++ project/ants/ants.py
@@ -25,6 +25,8 @@ class Place(object):
         # Phase 1: Add an entrance to the exit
         # BEGIN Problem 2
         "*** YOUR CODE HERE ***"
+        if exit is not None:
+            exit.entrance = self
         # END Problem 2
 
     def add_insect(self, insect):
